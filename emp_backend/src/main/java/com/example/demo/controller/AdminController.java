d7b14e64a56d30c345bed748e5457fa1849a351b
mayur <kumbharemayur75@gmail.com>
2025-06-15 10:44:23 +0530

    Api docs

---
+++ emp_backend/src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.AdminModel;
+import com.example.demo.repository.AdminRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class AdminController {
+
+	@Autowired
+	private AdminRepository repo;
+
+	// get all
+
+	@GetMapping("/admin")
+	public List<AdminModel> getAllAdmins() {
+		return repo.findAll();
+	}
+}
