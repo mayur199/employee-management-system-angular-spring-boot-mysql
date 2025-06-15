8e6bb90136a578a2e756458468013c89c944255b
mayur <kumbharemayur75@gmail.com>
2025-06-15 12:26:31 +0530

    Add Javadoc comments to EmpBackendApplication for better documentation

--- emp_backend/src/main/java/com/example/demo/EmpBackendApplication.java
+++ emp_backend/src/main/java/com/example/demo/EmpBackendApplication.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+/**
+ * The type Emp backend application.
+ */
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:4200")
 public class EmpBackendApplication {
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EmpBackendApplication.class, args);
 	}
